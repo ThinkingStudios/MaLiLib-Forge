11245b7cbd24ad2ad05a9fe678b71df11d7d930c
TexTrue <65154269+TexBlock@users.noreply.github.com>
2023-04-21 16:52:20 +0800

    Update MaLiLibForge.java

--- src/main/java/fi/dy/masa/malilib/MaLiLibForge.java
+++ src/main/java/fi/dy/masa/malilib/MaLiLibForge.java
@@ -2,7 +2,9 @@ package fi.dy.masa.malilib;
 
 import net.minecraftforge.client.ConfigGuiHandler;
 import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
 
+@Mod(MaLiLibReference.MOD_ID)
 public class MaLiLibForge {
     public MaLiLibForge() {
         ModLoadingContext.get().registerExtensionPoint(
