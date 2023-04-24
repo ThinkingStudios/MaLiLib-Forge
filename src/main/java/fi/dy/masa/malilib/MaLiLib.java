d06c416345fb2910ddb2fce921e4154f678bc73c
TexTrue <3140846162@qq.com>
2023-04-25 06:31:38 +0800

    try fix mixin, New icon?

--- src/main/java/fi/dy/masa/malilib/MaLiLib.java
+++ src/main/java/fi/dy/masa/malilib/MaLiLib.java
@@ -1,17 +1,26 @@
 package fi.dy.masa.malilib;
 
+import net.minecraftforge.client.ConfigGuiHandler;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import fi.dy.masa.malilib.event.InitializationHandler;
-import net.fabricmc.api.ModInitializer;
 
-public class MaLiLib implements ModInitializer
-{
+@Mod(MaLiLibReference.MOD_ID)
+public class MaLiLib {
     public static final Logger logger = LogManager.getLogger(MaLiLibReference.MOD_ID);
 
-    @Override
-    public void onInitialize()
-    {
+    public MaLiLib() {
         InitializationHandler.getInstance().registerInitializationHandler(new MaLiLibInitHandler());
+
+        ModLoadingContext.get().registerExtensionPoint(
+                ConfigGuiHandler.ConfigGuiFactory.class,
+                () -> new ConfigGuiHandler.ConfigGuiFactory((minecraftClient, screen) -> {
+                    MaLiLibConfigGui gui = new MaLiLibConfigGui();
+                    gui.setParent(screen);
+                    return gui;
+                })
+        );
     }
 }
--- src/main/java/fi/dy/masa/malilib/mixin/MixinInGameHud.java
+++ src/main/java/fi/dy/masa/malilib/mixin/MixinInGameHud.java
@@ -1,24 +1,20 @@
 package fi.dy.masa.malilib.mixin;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraftforge.client.gui.ForgeIngameGui;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.util.math.MatrixStack;
 import fi.dy.masa.malilib.event.RenderEventHandler;
 
 @Mixin(ForgeIngameGui.class)
-public abstract class MixinInGameHud
-{
-    @Shadow @Final private MinecraftClient client;
+public abstract class MixinInGameHud {
 
     @Inject(method = "render", at = @At("RETURN"))
     private void onGameOverlayPost(MatrixStack matrixStack, float partialTicks, CallbackInfo ci)
     {
-        ((RenderEventHandler) RenderEventHandler.getInstance()).onRenderGameOverlayPost(matrixStack, this.client, partialTicks);
+        ((RenderEventHandler) RenderEventHandler.getInstance()).onRenderGameOverlayPost(matrixStack, MinecraftClient.getInstance(), partialTicks);
     }
 }
