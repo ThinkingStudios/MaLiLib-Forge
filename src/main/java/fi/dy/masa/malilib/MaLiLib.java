940d2ffdf6390631b79693b934717a3e60ddc34c
TexTrue <3140846162@qq.com>
2023-04-25 07:52:00 +0800

    try fix mixin, New icon?

--- src/main/java/fi/dy/masa/malilib/MaLiLib.java
+++ src/main/java/fi/dy/masa/malilib/MaLiLib.java
@@ -1,15 +1,26 @@
 package fi.dy.masa.malilib;
 
+import net.minecraftforge.client.ConfigScreenHandler;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import fi.dy.masa.malilib.event.InitializationHandler;
-import net.fabricmc.api.ModInitializer;
 
-public class MaLiLib implements ModInitializer {
+@Mod(MaLiLibReference.MOD_ID)
+public class MaLiLib {
     public static final Logger logger = LogManager.getLogger(MaLiLibReference.MOD_ID);
 
-    @Override
-    public void onInitialize() {
+    public void MaLiLib() {
         InitializationHandler.getInstance().registerInitializationHandler(new MaLiLibInitHandler());
+
+        ModLoadingContext.get().registerExtensionPoint(
+                ConfigScreenHandler.ConfigScreenFactory.class,
+                () -> new ConfigScreenHandler.ConfigScreenFactory((minecraftClient, screen) -> {
+                    MaLiLibConfigGui gui = new MaLiLibConfigGui();
+                    gui.setParent(screen);
+                    return gui;
+                })
+        );
     }
 }
