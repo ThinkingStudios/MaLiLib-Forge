2dad50dde2be2a2ddb2af1727862246280d13e4d
TexTrue <3140846162@qq.com>
2023-04-30 09:10:59 +0800

    fix MixinInGameHud

--- src/main/java/fi/dy/masa/malilib/interfaces/IRenderDispatcher.java
+++ src/main/java/fi/dy/masa/malilib/interfaces/IRenderDispatcher.java
@@ -3,21 +3,21 @@ package fi.dy.masa.malilib.interfaces;
 public interface IRenderDispatcher
 {
     /**
-     * Registers a renderer which will have its {@link IRenderer.onRenderGameOverlayPost}
+     * Registers a renderer which will have its {@link IRenderer#onRenderGameOverlayPost}
      * method called after the vanilla rendering is done
      * @param renderer
      */
     void registerGameOverlayRenderer(IRenderer renderer);
 
     /**
-     * Registers a renderer which will have its {@link IRenderer.onRenderTooltipLast}
+     * Registers a renderer which will have its {@link IRenderer#onRenderTooltipLast}
      * method called after the vanilla tooltip text has been rendered.
      * @param renderer
      */
     void registerTooltipLastRenderer(IRenderer renderer);
 
     /**
-     * Registers a renderer which will have its {@link IRenderer.onRenderWorldLast}
+     * Registers a renderer which will have its {@link IRenderer#onRenderWorldLast}
      * method called after the vanilla rendering is done
      * @param renderer
      */
--- src/main/java/fi/dy/masa/malilib/mixin/MixinInGameHud.java
+++ src/main/java/fi/dy/masa/malilib/mixin/MixinInGameHud.java
@@ -1,24 +1,20 @@
 package fi.dy.masa.malilib.mixin;
 
-import org.spongepowered.asm.mixin.Final;
+import net.minecraftforge.client.gui.overlay.ForgeGui;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
 import fi.dy.masa.malilib.event.RenderEventHandler;
 
-@Mixin(InGameHud.class)
+@Mixin(ForgeGui.class)
 public abstract class MixinInGameHud
 {
-    @Shadow @Final private MinecraftClient client;
-
     @Inject(method = "render", at = @At("RETURN"))
     private void onGameOverlayPost(MatrixStack matrixStack, float partialTicks, CallbackInfo ci)
     {
-        ((RenderEventHandler) RenderEventHandler.getInstance()).onRenderGameOverlayPost(matrixStack, this.client, partialTicks);
+        ((RenderEventHandler) RenderEventHandler.getInstance()).onRenderGameOverlayPost(matrixStack, MinecraftClient.getInstance(), partialTicks);
     }
 }
