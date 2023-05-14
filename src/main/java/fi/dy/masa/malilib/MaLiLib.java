a74392e984ca10b5170ea05fa25be257ab458733
TexTrue <3140846162@qq.com>
2023-05-14 09:23:41 +0800

    fix

--- src/main/java/fi/dy/masa/malilib/MaLiLib.java
+++ src/main/java/fi/dy/masa/malilib/MaLiLib.java
@@ -3,7 +3,6 @@ package fi.dy.masa.malilib;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
