2c9081530ae0dcad5b2c8a640fd0a068154a57a8
TexTrue <3140846162@qq.com>
2023-06-10 20:50:40 +0800

    fix

--- src/main/java/fi/dy/masa/malilib/mixin/MixinInGameHud.java
+++ src/main/java/fi/dy/masa/malilib/mixin/MixinInGameHud.java
@@ -1,12 +1,14 @@
 package fi.dy.masa.malilib.mixin;
 
 import net.minecraftforge.client.gui.overlay.ForgeGui;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.util.math.MatrixStack;
+
 import fi.dy.masa.malilib.event.RenderEventHandler;
 
 @Mixin(ForgeGui.class)
