215470919af77770ca595d10857f7d77edc15edb
TexTrue <65154269+TexBlock@users.noreply.github.com>
2023-06-12 19:55:03 +0800

    fix

--- src/main/java/fi/dy/masa/malilib/MaLiLib.java
+++ src/main/java/fi/dy/masa/malilib/MaLiLib.java
@@ -1,6 +1,5 @@
 package fi.dy.masa.malilib;
 
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
