f7fc6d249361186862e17bd2f12abf9dfcec22bc
TexTrue <3140846162@qq.com>
2023-06-12 15:40:08 +0800

    fix

--- src/main/java/fi/dy/masa/malilib/MaLiLib.java
+++ src/main/java/fi/dy/masa/malilib/MaLiLib.java
@@ -1,9 +1,14 @@
 package fi.dy.masa.malilib;
 
-import net.minecraftforge.fml.common.Mod;
-
 import fi.dy.masa.malilib.compat.forge.ForgePlatformCompat;
 import fi.dy.masa.malilib.event.InitializationHandler;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,8 +18,19 @@ public class MaLiLib {
     public static final Logger logger = LoggerFactory.getLogger(MaLiLibReference.MOD_ID);
 
     public MaLiLib() {
-        InitializationHandler.getInstance().registerInitializationHandler(new MaLiLibInitHandler());
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        modEventBus.addListener(this::onInitialize);
+        modEventBus.addListener(this::onInitializeClient);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    public void onInitialize(FMLCommonSetupEvent event) {
+        InitializationHandler.getInstance().registerInitializationHandler(new MaLiLibInitHandler());
+    }
+
+    public void onInitializeClient(FMLClientSetupEvent event) {
         ForgePlatformCompat.getInstance().getMod(MaLiLibReference.MOD_ID).registerModConfigScreen((screen) -> {
             MaLiLibConfigGui gui = new MaLiLibConfigGui();
             gui.setParent(screen);
