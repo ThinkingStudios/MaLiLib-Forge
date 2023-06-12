62d839b4636b7520e4d72c699d167474c4f431cf
TexTrue <65154269+TexBlock@users.noreply.github.com>
2023-06-12 18:51:02 +0800

    fix

--- src/main/java/fi/dy/masa/malilib/MaLiLib.java
+++ src/main/java/fi/dy/masa/malilib/MaLiLib.java
@@ -3,7 +3,6 @@ package fi.dy.masa.malilib;
 import fi.dy.masa.malilib.compat.forge.ForgePlatformCompat;
 import fi.dy.masa.malilib.event.InitializationHandler;
 
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
