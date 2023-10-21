5f303560f9d0ca398c2efebba066e02d2d661b49
TexTrue <65154269+TexBlock@users.noreply.github.com>
2023-10-21 21:47:06 +0800

    fix icon missing issue

--- src/main/java/fi/dy/masa/malilib/MaLiLibReference.java
+++ src/main/java/fi/dy/masa/malilib/MaLiLibReference.java
@@ -1,7 +1,11 @@
 package fi.dy.masa.malilib;
 
+import fi.dy.masa.malilib.util.StringUtils;
+
 public class MaLiLibReference
 {
     public static final String MOD_ID = "mafglib";
+    public static final String NAMESPACE = "malilib";
     public static final String MOD_NAME = "MaFgLib";
+    public static final String MOD_VERSION = StringUtils.getModVersionString(MOD_ID);
 }
