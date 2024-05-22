6c0aa58c29d67feb605e15ef4db11fbc9ec9c0df
TexTrue <65154269+TexBlock@users.noreply.github.com>
2024-05-22 20:40:14 +0800

    Update MaLiLib.java

--- src/main/java/fi/dy/masa/malilib/MaLiLib.java
+++ src/main/java/fi/dy/masa/malilib/MaLiLib.java
@@ -1,11 +1,9 @@
 package fi.dy.masa.malilib;
 
-import net.minecraftforge.fml.common.Mod;
 import fi.dy.masa.malilib.event.InitializationHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Mod(MaLiLibReference.MOD_ID)
 public class MaLiLib {
     public static final Logger logger = LoggerFactory.getLogger(MaLiLibReference.MOD_ID);
 
