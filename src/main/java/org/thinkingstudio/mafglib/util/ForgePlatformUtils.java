b2f38cc570b4c1537fca2d7abf4f0158164f32f7
TexTrue <3140846162@qq.com>
2024-05-22 22:16:43 +0800

    clean

--- src/main/java/org/thinkingstudio/mafglib/util/ForgePlatformUtils.java
+++ src/main/java/org/thinkingstudio/mafglib/util/ForgePlatformUtils.java
@@ -1,25 +1,14 @@
 package org.thinkingstudio.mafglib.util;
 
-import org.thinkingstudio.mafglib.util.register.ModConfigScreenRegister;
-import org.thinkingstudio.mafglib.util.register.impl.ModConfigScreenRegisterImpl;
 import net.neoforged.fml.IExtensionPoint;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.neoforge.client.ConfigScreenHandler;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 public class ForgePlatformUtils {
-    private static final Map<String, ModConfigScreenRegister> mods = new ConcurrentHashMap<>();
     private final ModLoadingContext context = ModLoadingContext.get();
 
     public static ForgePlatformUtils getInstance() {
         return new ForgePlatformUtils();
-    }
-
-    @Deprecated
-    public ModConfigScreenRegister getMod(String id) {
-        return mods.computeIfAbsent(id, ModConfigScreenRegisterImpl::new);
     }
 
     public void getClientModIgnoredServerOnly() {
