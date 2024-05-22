1e700b32ba1f861d5065acb94d93487e374c5be2
TexTrue <3140846162@qq.com>
2024-05-22 10:02:34 +0800

    clean

--- src/main/java/fi/dy/masa/malilib/compat/neoforge/ForgePlatformUtils.java
+++ src/main/java/fi/dy/masa/malilib/compat/neoforge/ForgePlatformUtils.java
@@ -2,24 +2,32 @@ package fi.dy.masa.malilib.compat.neoforge;
 
 import fi.dy.masa.malilib.compat.neoforge.register.ModConfigScreenRegister;
 import fi.dy.masa.malilib.compat.neoforge.register.impl.ModConfigScreenRegisterImpl;
+import net.minecraft.client.gui.screen.Screen;
 import net.neoforged.fml.IExtensionPoint;
 import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.neoforge.client.ConfigScreenHandler;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ForgePlatformUtils {
     private static final Map<String, ModConfigScreenRegister> mods = new ConcurrentHashMap<>();
+    private final ModLoadingContext context = ModLoadingContext.get();
 
     public static ForgePlatformUtils getInstance() {
         return new ForgePlatformUtils();
     }
 
+    @Deprecated
     public ModConfigScreenRegister getMod(String id) {
         return mods.computeIfAbsent(id, ModConfigScreenRegisterImpl::new);
     }
 
     public void getClientModIgnoredServerOnly() {
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> IExtensionPoint.DisplayTest.IGNORESERVERONLY, (a, b) -> true));
+        context.registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> IExtensionPoint.DisplayTest.IGNORESERVERONLY, (a, b) -> true));
+    }
+
+    public void registerModConfigScreen(ModConfigScreenRegister.ModConfigScreenProvider configScreenProvider) {
+        context.registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory((client, screen) -> configScreenProvider.provide(screen)));
     }
 }
