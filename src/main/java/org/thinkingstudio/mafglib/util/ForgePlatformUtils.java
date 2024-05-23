edfe8971a07495a1cacd918920af29c59b1e3146
TexTrue <3140846162@qq.com>
2024-05-23 16:21:37 +0800

    fix `registerModConfigScreen`

--- src/main/java/org/thinkingstudio/mafglib/util/ForgePlatformUtils.java
+++ src/main/java/org/thinkingstudio/mafglib/util/ForgePlatformUtils.java
@@ -2,6 +2,7 @@ package org.thinkingstudio.mafglib.util;
 
 import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.network.NetworkConstants;
 
@@ -16,7 +17,7 @@ public class ForgePlatformUtils {
         context.registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
 
-    public void registerModConfigScreen(ModConfigScreenProvider configScreenProvider) {
-        context.registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory((client, screen) -> configScreenProvider.provide(screen)));
+    public void registerModConfigScreen(String modid, ModConfigScreenProvider configScreenProvider) {
+        ModList.get().getModContainerById(modid).orElseThrow().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory((client, screen) -> configScreenProvider.provide(screen)));
     }
 }
