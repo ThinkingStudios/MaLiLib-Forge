bf6a3dfd38903fb2845e7c86d407819f65a3bb01
TexTrue <3140846162@qq.com>
2024-07-25 00:09:13 +0800

    little rewrite `NeoUtils`

--- src/main/java/org/thinkingstudio/mafglib/util/NeoUtils.java
+++ src/main/java/org/thinkingstudio/mafglib/util/NeoUtils.java
@@ -1,16 +1,16 @@
 package org.thinkingstudio.mafglib.util;
 
 import net.minecraft.client.gui.screen.Screen;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+import java.util.function.Function;
 
 public class NeoUtils {
     private static NeoUtils INSTANCE;
 
-    public void registerModConfigScreen(ModContainer modContainer, ConfigScreenProvider configScreenProvider) {
-        modContainer.registerExtensionPoint(IConfigScreenFactory.class, (container, screen) -> configScreenProvider.provide(screen));
+    public void registerModConfigScreen(ModContainer modContainer, Function<Screen, Screen> screenFunction) {
+        modContainer.registerExtensionPoint(IConfigScreenFactory.class, (container, screen) -> screenFunction.apply(screen));
     }
 
     public static NeoUtils getInstance() {
@@ -18,11 +18,5 @@ public class NeoUtils {
             INSTANCE = new NeoUtils();
         }
         return INSTANCE;
-    }
-
-    @OnlyIn(Dist.CLIENT)
-    @FunctionalInterface
-    public interface ConfigScreenProvider {
-        Screen provide(Screen parent);
     }
 }
