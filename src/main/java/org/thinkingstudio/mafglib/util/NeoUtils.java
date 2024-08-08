1482f233e88489ea6b48379cfca46e757c892a84
TexTrue <3140846162@qq.com>
2024-08-08 15:00:26 +0800

    clean

--- src/main/java/org/thinkingstudio/mafglib/util/NeoUtils.java
+++ src/main/java/org/thinkingstudio/mafglib/util/NeoUtils.java
@@ -4,7 +4,11 @@ import net.minecraft.client.gui.screen.Screen;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.loading.FMLLoader;
+import net.neoforged.fml.loading.moddiscovery.ModInfo;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+import org.apache.maven.artifact.versioning.ArtifactVersion;
+import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 
 import java.util.function.Function;
 
@@ -18,6 +22,16 @@ public class NeoUtils {
 
     public void registerConfigScreen(ModContainer modContainer, Function<Screen, Screen> screenFunction) {
         modContainer.registerExtensionPoint(IConfigScreenFactory.class, (container, screen) -> screenFunction.apply(screen));
+    }
+
+    public ArtifactVersion getModVersion(String modId) {
+        for(ModInfo modInfo: FMLLoader.getLoadingModList().getMods()) {
+            if(modInfo.getModId().equals(modId)) {
+                return modInfo.getVersion();
+            }
+        }
+
+        return new DefaultArtifactVersion("?");
     }
 
     public static NeoUtils getInstance() {
