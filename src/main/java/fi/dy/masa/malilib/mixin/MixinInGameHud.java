8704312ca4eed1d1a3bf5eda0910910b19af9795
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-02-26 18:52:29 +0800

    fix GuiLayer!

--- src/main/java/fi/dy/masa/malilib/mixin/MixinInGameHud.java
+++ src/main/java/fi/dy/masa/malilib/mixin/MixinInGameHud.java
@@ -1,6 +1,8 @@
 package fi.dy.masa.malilib.mixin;
 
 import net.minecraft.client.gui.LayeredDrawer;
+import net.neoforged.neoforge.client.gui.GuiLayerManager;
+import net.neoforged.neoforge.client.gui.VanillaGuiLayers;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -18,19 +20,22 @@ import fi.dy.masa.malilib.event.RenderEventHandler;
 public abstract class MixinInGameHud
 {
     @Shadow @Final private MinecraftClient client;
-    @Shadow @Final private LayeredDrawer layeredDrawer;
+    //@Shadow @Final private LayeredDrawer layeredDrawer;
+
+    @Shadow @Final private GuiLayerManager layerManager;
 
     @Inject(method = "<init>", at = @At("TAIL"))
     private void onInit(CallbackInfo info)
     {
-        this.layeredDrawer.addLayer(this::malilib_renderGameOverlayLastDrawer);
+        this.layerManager.add(VanillaGuiLayers.SUBTITLE_OVERLAY, this::malilib_renderGameOverlayLastDrawer);
+        //this.layeredDrawer.addLayer(this::malilib_renderGameOverlayLastDrawer);
     }
 
-//    @Inject(method = "render", at = @At(value = "RETURN"))
-//    private void malilib_onGameOverlayPost(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci)
-//    {
-//        ((RenderEventHandler) RenderEventHandler.getInstance()).onRenderGameOverlayPost(context, this.client, tickCounter.getTickDelta(false));
-//    }
+    @Inject(method = "render", at = @At(value = "RETURN"))
+    private void malilib_onGameOverlayPost(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci)
+    {
+        ((RenderEventHandler) RenderEventHandler.getInstance()).onRenderGameOverlayPost(context, this.client, tickCounter.getTickDelta(false));
+    }
 
     @Unique
     private void malilib_renderGameOverlayLastDrawer(DrawContext context, RenderTickCounter tickCounter)
--- src/main/java/org/thinkingstudio/mafglib/MaFgLib.java
+++ src/main/java/org/thinkingstudio/mafglib/MaFgLib.java
@@ -3,30 +3,20 @@ package org.thinkingstudio.mafglib;
 import fi.dy.masa.malilib.MaLiLib;
 import fi.dy.masa.malilib.MaLiLibReference;
 import fi.dy.masa.malilib.compat.modmenu.ModMenuImpl;
-import fi.dy.masa.malilib.event.RenderEventHandler;
-import net.minecraft.client.MinecraftClient;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.EventPriority;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLLoader;
-import net.neoforged.neoforge.client.event.RenderGuiLayerEvent;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
-import net.neoforged.neoforge.common.NeoForge;
 
 @Mod(value = MaLiLibReference.MOD_ID, dist = Dist.CLIENT)
 public class MaFgLib {
-    public MaFgLib(ModContainer modContainer) {
+    public MaFgLib(ModContainer modContainer, IEventBus modEventBus) {
         if (FMLLoader.getDist().isClient()) {
             modContainer.registerExtensionPoint(IConfigScreenFactory.class, new ModMenuImpl().getModConfigScreenFactory());
 
             MaLiLib.onInitialize();
-
-            NeoForge.EVENT_BUS.addListener(EventPriority.HIGHEST, RenderGuiLayerEvent.Post.class, event -> {
-                ((RenderEventHandler) RenderEventHandler.getInstance()).onRenderGameOverlayPost(
-                        event.getGuiGraphics(), MinecraftClient.getInstance(), event.getPartialTick().getTickDelta(false)
-                );
-            });
         }
     }
 }
