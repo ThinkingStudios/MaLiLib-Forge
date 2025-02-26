acb35f5e9837aabc9b0987a3b11afa020b063812
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-02-26 19:03:42 +0800

    clean

--- src/main/java/org/thinkingstudio/mafglib/MaFgLib.java
+++ src/main/java/org/thinkingstudio/mafglib/MaFgLib.java
@@ -4,7 +4,6 @@ import fi.dy.masa.malilib.MaLiLib;
 import fi.dy.masa.malilib.MaLiLibReference;
 import fi.dy.masa.malilib.compat.modmenu.ModMenuImpl;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLLoader;
@@ -12,7 +11,7 @@ import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 @Mod(value = MaLiLibReference.MOD_ID, dist = Dist.CLIENT)
 public class MaFgLib {
-    public MaFgLib(ModContainer modContainer, IEventBus modEventBus) {
+    public MaFgLib(ModContainer modContainer) {
         if (FMLLoader.getDist().isClient()) {
             modContainer.registerExtensionPoint(IConfigScreenFactory.class, new ModMenuImpl().getModConfigScreenFactory());
 
