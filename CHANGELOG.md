c519633337bfcd625e0840d9e4305b932a09b838
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-04-20 10:44:44 +0800

    remove `ModInitializer`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,2 @@
 ## Change
-- rework entrypoint, now we use neoforge's ExtensionPoint api
+- remove `ModInitializer`
--- src/main/java/org/thinkingstudio/mafglib/MaFgLib.java
+++ src/main/java/org/thinkingstudio/mafglib/MaFgLib.java
@@ -11,7 +11,6 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLLoader;
 import org.thinkingstudio.mafglib.loader.entrypoints.ConfigScreenEntrypoint;
 import org.thinkingstudio.mafglib.loader.entrypoints.EntrypointHandler;
-import org.thinkingstudio.mafglib.loader.entrypoints.ModInitializer;
 
 @Mod(value = MaLiLibReference.MOD_ID, dist = Dist.CLIENT)
 public class MaFgLib {
@@ -19,7 +18,7 @@ public class MaFgLib {
         EntrypointHandler.init(modEventBus);
         if (FMLLoader.getDist().isClient()) {
             modContainer.registerExtensionPoint(ConfigScreenEntrypoint.class, new ModMenuImpl());
-            modContainer.registerExtensionPoint(ModInitializer.class, new MaLiLib());
+            MaLiLib.onInitialize();
             MaLiLibDataGen.onInitializeDataGenerator(modEventBus);
         }
     }
