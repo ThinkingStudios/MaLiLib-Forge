1d83ead23f485cdf075dfb429baae57d5322275b
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-04-20 10:34:44 +0800

    clean

--- src/main/java/org/thinkingstudio/mafglib/loader/entrypoints/EntrypointHandler.java
+++ src/main/java/org/thinkingstudio/mafglib/loader/entrypoints/EntrypointHandler.java
@@ -2,7 +2,7 @@ package org.thinkingstudio.mafglib.loader.entrypoints;
 
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModList;
-import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.loading.FMLLoader;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 public class EntrypointHandler {
     public static void init(IEventBus modEventBus) {
-        modEventBus.addListener(FMLLoadCompleteEvent.class, event -> {
+        modEventBus.addListener(FMLClientSetupEvent.class, event -> {
             ModList.get().forEachModContainer((modId, container) -> {
                 if (FMLLoader.getDist().isClient()) {
                     Optional<ConfigScreenEntrypoint> configScreen = container.getCustomExtension(ConfigScreenEntrypoint.class);
